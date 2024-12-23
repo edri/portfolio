644a9538d9911d9af15484885dd3e4dc239a8331
edri <mig.santamar@gmail.com>
2024-12-23 16:31:19 +0100

    Add tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <>
-      <div>YAAAAAY</div>
+      <section className="h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </section>
     </>
   );
 }
